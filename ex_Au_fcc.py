a12e094286899272335af42f66639e471d079f8f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-05-02 11:15:07 +0700

    Simplify

--- ex_Au_fcc.py
+++ ex_Au_fcc.py
@@ -2,14 +2,16 @@ from ase import *
 from hotbit import *
 from ase.lattice.cubic import FaceCenteredCubic
 import numpy as np
-import pylab
-from box.interpolation import interpolate_path
+
 pi = np.pi
 
 d = 4.08
-atoms = FaceCenteredCubic('Au', latticeconstant=d,
-                          directions=((0,1,1),(1,0,1),(1,1,0)),
-                          align=False)
+atoms = FaceCenteredCubic(
+    'Au',
+    latticeconstant=d,
+    directions=((0,1,1),(1,0,1),(1,1,0)),
+    align=False
+)
                           
 calc = Hotbit(SCC=False, kpts=(8,8,8), txt=None)
 atoms.set_calculator(calc)
